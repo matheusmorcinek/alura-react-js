77d4d59115843b82b27332da89d9f8420ccb670f
Matheus Morcinek <matheus.morcinek@Matheuss-MacBook-Pro.local>
2020-10-08 13:56:34 -0300

    controlando o estado e a renderização

--- ceep-app/src/App.js
+++ ceep-app/src/App.js
@@ -4,11 +4,41 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
+
+  //props, state
+  constructor() {
+
+    super();
+
+    this.state = {
+      notas: []
+    };
+  }
+
+  criarNota(titulo, texto) {
+
+    const novaNota = {
+      titulo,
+      texto
+    };
+
+    const novoArrayNotas = [...this.state.notas, novaNota];
+
+    const novoEstado = {
+      notas: novoArrayNotas
+    };
+
+    this.setState(novoEstado);
+
+    console.log(`Uma nova nota foi criada ${titulo} ${texto}`);
+  }
+
   render() {
+
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
       </section>
     );
   }
